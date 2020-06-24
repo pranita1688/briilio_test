9379745b37de6a4220cc24e95f75b68fc8c57cb1
DELL <pranita1688@gmail.com>
2020-06-24 20:58:44 +0530

    Online Room Booking

--- src/test/java/meetingroomapplicationtest/MeetingRoomApplicationTests.java
+++ src/test/java/meetingroomapplicationtest/MeetingRoomApplicationTests.java
@@ -1,4 +1,4 @@
-package mrs;
+package meetingroomapplicationtest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
-public class MrsApplicationTests {
+public class MeetingRoomApplicationTests {
 
     @Test
     public void contextLoads() {
